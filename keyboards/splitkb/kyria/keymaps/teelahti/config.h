0466e62f93fd430f2923bc7d3adbee78697731d1
Tero Teelahti <tero@teelahti.fi>
2024-01-25 13:23:38 +0200

    Add Kyria Rev1

--- keyboards/splitkb/kyria/keymaps/teelahti/config.h
+++ keyboards/splitkb/kyria/keymaps/teelahti/config.h
@@ -5,8 +5,8 @@
 #endif
 
 // On Liatris microcontroller use the power led as caps lock indicator instead of 
-// having it shining bright all the time. Not tested whether this breaks something
-// when using with other microcontrollers.
+// having it shining bright all the time. 
 // See https://docs.splitkb.com/hc/en-us/articles/5799711553820-Power-LED
+// Warning: these will break Elite-C based Kyria
 #define LED_CAPS_LOCK_PIN 24
 #define LED_PIN_ON_STATE 0
