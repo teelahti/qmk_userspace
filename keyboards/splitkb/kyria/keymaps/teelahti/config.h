cbb5b0639fd955e3565c98593a61f2fca108b9c0
Tero Teelahti <tero@teelahti.fi>
2025-08-19 11:52:09 +0300

    Formatting change

--- keyboards/splitkb/kyria/keymaps/teelahti/config.h
+++ keyboards/splitkb/kyria/keymaps/teelahti/config.h
@@ -1,11 +1,11 @@
 #pragma once
 
 #ifdef OLED_ENABLE
-  #define OLED_DISPLAY_128X64
+#    define OLED_DISPLAY_128X64
 #endif
 
-// On Liatris microcontroller use the power led as caps lock indicator instead of 
-// having it shining bright all the time. 
+// On Liatris microcontroller use the power led as caps lock indicator instead of
+// having it shining bright all the time.
 // See https://docs.splitkb.com/hc/en-us/articles/5799711553820-Power-LED
 // Warning: these will break Elite-C based Kyria
 #define LED_CAPS_LOCK_PIN 24
