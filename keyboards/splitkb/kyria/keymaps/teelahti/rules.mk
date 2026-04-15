6643a53eff683daeaae51c6dc8cfc7f6d3e49f49
Tero Teelahti <tero@teelahti.fi>
2026-04-15 16:12:44 +0300

    Update packages Disable mouse keys from office keyboard

--- keyboards/splitkb/kyria/keymaps/teelahti/rules.mk
+++ keyboards/splitkb/kyria/keymaps/teelahti/rules.mk
@@ -1,3 +1,4 @@
 OLED_ENABLE = yes   # Enables the use of OLED displays
-MOUSEKEY_ENABLE = yes
+# Disable for work keyboard. New home keyboard is Halcyon and separate, if old one is flashed this is needed
+MOUSEKEY_ENABLE = false
 
